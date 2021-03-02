ff4d2cd5f7ea6d2316e142188b1c020a77d23489
Michał Wilk <dev2theworld@gmail.com>
2021-03-02 15:52:27 +0100

    Mouse & Touch event added

--- js/main.js
+++ js/main.js
@@ -10,6 +10,17 @@ let isDraggins = false,
 
   slides.forEach((slide, index) => {
 
+    // Touch events
+    slide.addEventListener('touchstart', touchStart(index));
+    slide.addEventListener('touchend', touchEnd);
+    slide.addEventListener('touchmove', touchMove);
+
+    // Mouse events
+    slide.addEventListener('mousedown', touchStart(index));
+    slide.addEventListener('mouseup', touchEnd);
+    slide.addEventListener('mouseleave', touchEnd);
+    slide.addEventListener('mousemove', touchMove);
+
     // preventing click & drag img effect
     const slideImage = slide.querySelector('img');
     slideImage.addEventListener('dragstart', (e) => e.preventDefaul());
